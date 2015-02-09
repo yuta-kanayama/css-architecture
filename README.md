b31d43bed1c34c8e7647324adf12ae631f91476c
Yuta Kanayama <kanayama.work@gmail.com>
2015-02-09 17:51:04 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CSS設計2015のサンプルファイル
 
+
+## BEM + Sass( Mixin )
+
+//Element
+@mixin e($name) {
+  @at-root &__#{$name} {
+    @content;
+  }
+}
+
+//Modifier
+@mixin m($name) {
+  @at-root &--#{$name} {
+    @content;
+  }
+}
+
--- config.rb
+++ config.rb
@@ -4,8 +4,10 @@ sass_dir = "public_html/assets/_scss"
 images_dir = "public_html/assets/img"
 javascripts_dir = "public_html/assets/js"
 
-output_style = :compressed
-#output_style = :expanded
+cache = true
+
+#output_style = :compressed
+output_style = :expanded
 
 line_comments = false
 #line_comments = true
